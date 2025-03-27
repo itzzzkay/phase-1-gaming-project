a4b6ba92b8006d10eb173fc1f789785af68d7af4
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-03-27 05:31:10 +0300

    function for obtaining game data

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 const URL = "http://localhost:3000/games"
 
-document.addEventListener("DOMContentLoaded", )
+document.addEventListener("DOMContentLoaded", getGameData)
+
+function getGameData() {
+    fetch(URL)
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error(response);
+        }
+        return res.json();
+    })
+    .then((games) => {
+        displayGames(games)
+    })
+}
+
